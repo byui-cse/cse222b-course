fd37861e912202933fbd2e7729e3ccf0479cd49f
Layne Moseley <laynemoseley@gmail.com>
2022-09-26 10:30:20 -0600

    week 3 updates

--- docs/week03/provided_solutions/task2/model.kt
+++ docs/week03/provided_solutions/task2/model.kt
@@ -12,7 +12,7 @@ enum class EmployeeType{
     PROGRAMER, DEVLEAD
 
 }
-/**This has to be done using Is_a type inheritance in order to work with the try-catch system*/
+/**This has to be done using IS-A type inheritance in order to work with the try-catch system*/
 class AssignmentException(message:String): Exception(message) {}
 
 open class Employee(var name:String, val hireDate: LocalDate, var yearsExperience:Short, val type:EmployeeType){
