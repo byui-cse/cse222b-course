b43140145fd755e2d424d22f25d38f1b516b7de8
Layne Moseley <laynemoseley@gmail.com>
2023-01-27 16:46:35 -0700

    done!

--- docs/week05/task-project/src/main/kotlin/tasks-completed.kt
+++ docs/week05/task-project/src/main/kotlin/tasks-completed.kt
@@ -1,149 +1,191 @@
-//import kotlin.random.Random
+////
+////  tasks.kt
+////  Week 5 Tasks
+////
+////  You need to write code to complete the functions below to complete each task.
+////
+////  task4() is meant to be open-ended. There are probably many ways to solve it. Use creativity
+////  to come to a good solution
+////
 //
-//enum class TestResult {
-//    SUCCESS,
-//    INVALID_INPUT
+//enum class IntOperationType {
+//    ADD, SUBTRACT, MULTIPLY, DIVIDE
 //}
+//
+//typealias IntOperation = (Int, Int) -> Int
+//
+//typealias IntOperationMap = Map<IntOperationType, IntOperation>
 //
 ////  Task 0
-////  Add code to this function using multiple statements to validate that
-////  the parameters comply with the following rules:
-////      aBool must be true
-////      int1 must be greater than or equal to 0
-////      int2 must be greater than int1
-////      aString must be exactly 4 characters long
-//fun task0(aBool: Boolean, int1: Int, int2: Int, aString: String): TestResult {
-//    if (!aBool) {
-//        return TestResult.INVALID_INPUT
-//    }
-//
-//    if (int1 < 0) {
-//        return TestResult.INVALID_INPUT
-//    }
-//
-//    if (int1 > int2) {
-//        return TestResult.INVALID_INPUT
-//    }
-//
-//    if (aString.length != 4) {
-//        return TestResult.INVALID_INPUT
-//    }
-//
-//    return TestResult.SUCCESS
+////
+////  Lambda functions can be stored in a Map, just like any other type of variable
+////
+////  In this task, create a Map that contains operations for all types in IntOperationType
+////  The key of the map will be an IntOperationType, and the value will be an IntOperation
+//fun task0(): IntOperationMap {
+//    return mapOf(IntOperationType.ADD to { a, b -> a + b },
+//        IntOperationType.SUBTRACT to { a, b -> a - b },
+//        IntOperationType.MULTIPLY to { a, b -> a * b },
+//        IntOperationType.DIVIDE to { a, b -> a / b })
 //}
 //
-//// Task 1
-//// Nullable types in Kotlin are a great way to ensure a value exists before you use it
-//// This function will return the length of an optional string
-//// If the string is null, return 0
-//// If the string is not null, return the length of the string
-//fun task1(aString: String?): Int {
-//    return aString?.length ?: 0
+//typealias IntResult = () -> Int
+//
+////  Task 1
+////
+////  Higher-order functions are functions that... wait for it... return another function
+////
+////  In this task, return a function that, when called, will add the two numbers passed _into_ the higher order function
+//fun task1(a: Int, b: Int): IntResult? {
+//    return { a + b }
 //}
 //
-//// Task 2
-//// Convert the input string into an integer. If the string cannot be converted into an integer, return  null
-//fun task2(aString: String): Int? {
-//    return aString.toIntOrNull()
+////  Task 2
+////
+////  Create a higher order function that simple executes the parameter that also is a function.
+////  This gets _really_ confusing, so here is the best description I can come up with:
+////      - task2 is a function, which has a single parameter: a function that returns an Int
+////      - task2 returns a function, that when called, will return an Int
+////      - So the return value of task2 is a function that should return the result of the parameter function
+////
+////  Is your head spinning yet?
+//fun task2(execute: () -> Int): IntResult? {
+//    return { execute() }
 //}
-//
 //
 ////  Task 3
-////  Built-in functions like map, filter, reduce and sort take a parameter that is a closure.
 ////
-////  Inside this function is an array of closures that each take two Int parameters. Complete each
-////  closure according to the requirements below.
-////
-////  Closure 0: return the sum of the two parameters
-////  Closure 1: return the product of the two parameters
-////  Closure 2: return -1 if the first parameter is less than the second parameter,
-////              return 0 if they are equal
-////              return 1 if the first parameter is greater than the second parameter
-////  Closure 3: return -1 if both parameters are odd
-////              return 1 if both parameters are even
-////              return 0 if one parameter is odd and the other is even
-//typealias task3Func = (Int, Int) -> Int
-//fun task3(): List<task3Func> {
-//    return listOf(fun (lhs: Int, rhs: Int): Int {
-//        return lhs + rhs
-//    },fun (lhs: Int, rhs: Int): Int {
-//        return lhs * rhs
-//    },fun (lhs: Int, rhs: Int): Int {
-//        if (lhs < rhs) return -1
-//        if (lhs > rhs) return 1
-//        return 0
-//    },fun (lhs: Int, rhs: Int): Int {
-//        if (lhs % 2 == 0 && rhs % 2 == 0) return 1
-//        if (lhs % 2 != 0 && rhs % 2 != 0) return -1
-//        return 0
-//    })
-//}
-//
-//// Task 4
-//// Using the built-in map function, convert each integer into a string
-//fun task4(input: List<Int>): List<String> {
-//    return input.map { it.toString() }
-//}
-//
-//// Task 5
-//// Using the built-in filter method, remove all odd values from the array
-//fun task5(input: List<Int>): List<Int> {
-//    return input.filter { it % 2 == 0 }
-//}
-//
-//// Task 6
-//// Using the built-in reduce method, return the sum of the input array
-//fun task6(input: List<Int>): Int {
-//    return input.reduce { acc, value -> acc + value }
-//}
-//
-//// Task 7
-//// Given the 2-dimensional input array, return the count of null values
-////
-//// Note: Both the sublists or the integers can be null
-//fun task7(input: List<List<Int?>?>): Int {
-//    var count = 0
-//
-//    for (list in input) {
-//        if (list == null) {
-//            count++
-//        } else {
-//            for (value in list) {
-//                if (value == null) {
-//                    count++
-//                }
-//            }
-//        }
+////  Like task2, you'll call the passed in function parameters and do an operation on them
+////  The operation you do needs to be based on the "op" parameter
+//fun task3(op: IntOperationType, first: () -> Int, second: () -> Int): IntResult? {
+//    if (op == IntOperationType.ADD) {
+//        return { first() + second() }
 //    }
 //
-//    return count
-//}
-//
-//// Task 8
-//// Return a 2-dimensional array with the appropriate rows and columns
-//// The max parameter specifies the  maximum value for the random numbers
-////
-//// Hint: https://kotlinlang.org/docs/ranges.html
-//fun task8(rows: Int, columns: Int, max: Int): List<List<Int>> {
-//    var list = mutableListOf<List<Int>>()
-//    var rnd = Random.Default
-//    for (row in 0 until rows) {
-//        var sublist = mutableListOf<Int>()
-//        for (col in 0 until columns) {
-//            sublist.add(rnd.nextInt(max))
-//        }
-//        list.add((sublist))
+//    if (op == IntOperationType.SUBTRACT) {
+//        return { first() - second() }
 //    }
 //
-//    return list
+//    if (op == IntOperationType.MULTIPLY) {
+//        return { first() * second() }
+//    }
+//
+//    return { first() / second() }
 //}
 //
-//// Task 9
-//// Using the built-in map function, transform each row of data into the average of the row
-//// The input 2-dimensional array is like a spreadsheet, with rows and columns
-//// We need the average of each row
+//class AnyValue {
+//
+//    private val value: Any
+//
+//    constructor(s: String) { value = s }
+//    constructor(i: Int) { value = i }
+//    constructor(d: Double) { value = d }
+//
+//    val isString: Boolean
+//        get() = stringValue() != null
+//
+//    val isInt: Boolean
+//        get() = intValue() != null
+//
+//    val isDouble: Boolean
+//        get() = doubleValue() != null
+//
+//    fun stringValue(): String? {
+//        if (value is String) {
+//            return value
+//        }
+//
+//        return null
+//    }
+//
+//    fun intValue(): Int? {
+//        if (value is Int) {
+//            return value
+//        }
+//
+//        return null
+//    }
+//
+//    fun doubleValue(): Double? {
+//        if (value is Double) {
+//            return value
+//        }
+//
+//        return null
+//    }
+//}
+//
+//enum class OperationType {
+//    ADD, SUBTRACT, CONCATENATE
+//}
+//
+//enum class OperationStatus {
+//    VALID, INVALID
+//}
+//
+////  Task 4
 ////
-//// Hint: List has a function called .average()
-//fun task9(data: List<List<Int>>): List<Double> {
-//    return data.map { it.average() }
+////  Lambda functions in Kotlin are first-class citizens, meaning they can be assigned
+////  to variables and passed as a parameter to a function.
+////
+////  For task0, you should return an OperationHandler with the following rules:
+////      - Int and Number types can be Added and Subtracted
+////      - String types can be concatenated
+////      - Strings _cannot_ be added and subtracted
+////      - Int and Number types _cannot_ be concatenated
+////      - If an Int and Double have an operation applied, the result should _be_ a Double
+////
+////  If the values can have an operation applied to them, return a OperationStatus.VALID, as well as the result
+////  If the values cannot have the operation applied, return OperationStatus.INVALID in the Pair, along with null
+////
+////  To make things easier to code, you should add several methods and computed properties to AnyValue,
+////  such as the following for string types:
+////      - fun stringValue(): String?
+////      - val isString: Boolean
+//
+//typealias OperationHandler = (OperationType, AnyValue, AnyValue) -> Pair<OperationStatus, AnyValue?>
+//fun task4(): OperationHandler? {
+//
+//    return { op: OperationType, a: AnyValue, b: AnyValue ->
+//
+//        var result: Pair<OperationStatus, AnyValue?> = Pair(OperationStatus.INVALID, null)
+//
+//        if (a.isString && b.isString && op == OperationType.CONCATENATE) {
+//            result = Pair(OperationStatus.VALID, AnyValue(a.stringValue() + b.stringValue()))
+//        }
+//
+//        if (a.isInt && b.isInt && op == OperationType.ADD) {
+//            result = Pair(OperationStatus.VALID, AnyValue((a.intValue() ?: 0) + (b.intValue() ?: 0)))
+//        }
+//
+//        if (a.isInt && b.isInt && op == OperationType.SUBTRACT) {
+//            result = Pair(OperationStatus.VALID, AnyValue((a.intValue() ?: 0) - (b.intValue() ?: 0)))
+//        }
+//
+//        if (a.isDouble && b.isDouble && op == OperationType.ADD) {
+//            result = Pair(OperationStatus.VALID, AnyValue((a.doubleValue() ?: 0.0) + (b.doubleValue() ?: 0.0)))
+//        }
+//
+//        if (a.isInt && b.isInt && op == OperationType.SUBTRACT) {
+//            result = Pair(OperationStatus.VALID, AnyValue((a.intValue() ?: 0) - (b.intValue() ?: 0)))
+//        }
+//
+//        if (a.isInt && b.isDouble && op == OperationType.ADD) {
+//            result = Pair(OperationStatus.VALID, AnyValue((a.intValue() ?: 0.0).toDouble() + (b.doubleValue() ?: 0.0)))
+//        }
+//
+//        if (a.isInt && b.isDouble && op == OperationType.SUBTRACT) {
+//            result = Pair(OperationStatus.VALID, AnyValue((a.intValue() ?: 0.0).toDouble() - (b.doubleValue() ?: 0.0)))
+//        }
+//
+//        if (a.isDouble && b.isInt && op == OperationType.ADD) {
+//            result = Pair(OperationStatus.VALID, AnyValue((a.doubleValue() ?: 0.0) + (b.intValue() ?: 0).toDouble()))
+//        }
+//
+//        if (a.isDouble && b.isInt && op == OperationType.SUBTRACT) {
+//            result = Pair(OperationStatus.VALID, AnyValue((a.doubleValue() ?: 0.0) - (b.intValue() ?: 0).toDouble()))
+//        }
+//
+//        result
+//    }
 //}
