036420f68d3ccd277260fe986fcfa712f364987e
Layne Moseley <laynemoseley@gmail.com>
2023-01-27 16:40:54 -0700

    week 5 done

--- docs/week05/task-project/src/main/kotlin/main.kt
+++ docs/week05/task-project/src/main/kotlin/main.kt
@@ -24,20 +24,78 @@ fun main(args: Array<String>) {
 }
 
 fun testTask0() {
+    val operations = task0()
+    val test1 = operations[IntOperationType.ADD]?.let { it(2, 2) }
+    check(test1 == 4) { "Make sure to add an addition function to the map"}
+
+    val test2 = operations[IntOperationType.SUBTRACT]?.let { it(2, 2) }
+    check(test2 == 0) { "Make sure to add an subtraction function to the map"}
+
+    val test3 = operations[IntOperationType.MULTIPLY]?.let { it(4, 4) }
+    check(test3 == 16) { "Make sure to add an multiply function to the map"}
+
+    val test4 = operations[IntOperationType.DIVIDE]?.let { it(4, 2) }
+    check(test4 == 2) { "Make sure to add an divide function to the map"}
 }
 
 fun testTask1() {
+    val add = task1(2, 2)
+    val test1 = add?.let { it() }
+    check(test1 == 4) { "Your function should add the two values together" }
 
+    val test2 = add?.let { it() }
+    check(test2 == 4) { "Your function should add the two values together" }
 }
 
 fun testTask2() {
+    val exec1 = task2 { 2 + 2 }
+    val test1 = exec1?.let { it() }
+    check(test1 == 4) { "Your function should simply call the parameter function and return its value" }
 
+    val exec2 = task2 { 2 * 4 }
+    val test2 = exec2?.let { it() }
+    check(test2 == 8) { "Your function should simply call the parameter function and return its value" }
 }
 
 fun testTask3() {
+    val test1 = task3(IntOperationType.ADD, { 2 }, { 2 })
+    check(test1 != null)
+    check(test1() == 4) { "Make sure to run the operation requested"}
 
+    val test2 = task3(IntOperationType.SUBTRACT, { 2 }, { 1 })
+    check(test2 != null)
+    check(test2() == 1) { "Make sure to run the operation requested"}
+
+    val test3 = task3(IntOperationType.MULTIPLY, { 3 }, { 2 })
+    check(test3 != null)
+    check(test3() == 6) { "Make sure to run the operation requested"}
+
+    val test4 = task3(IntOperationType.DIVIDE, { 4 }, { 2 })
+    check(test4 != null)
+    check(test4() == 2) { "Make sure to run the operation requested"}
 }
 
 fun testTask4() {
+    val operation = task4()
+    check(operation != null) { "Please return an operation lambda from task0"}
+    val test1 = operation(OperationType.ADD, AnyValue(2), AnyValue(2))
+    check(test1.first == OperationStatus.VALID && test1.second?.intValue() == 4) { "2 + 2 is 4" }
 
+    val test2 = operation(OperationType.ADD, AnyValue(2.75), AnyValue(2.75))
+    check(test2.first == OperationStatus.VALID && test2.second?.doubleValue() == 5.5) { "2.75 + 2.75 is 5.5" }
+
+    val test3 = operation(OperationType.CONCATENATE, AnyValue("Merry"), AnyValue(" Christmas"))
+    check(test3.first == OperationStatus.VALID && test3.second?.stringValue() == "Merry Christmas") { "Make sure to concat the string values" }
+
+    val test4 = operation(OperationType.ADD, AnyValue(1), AnyValue(2.75))
+    check(test4.first == OperationStatus.VALID && test4.second?.doubleValue() == 3.75) { "1 + 2.75 is 3.75" }
+
+    val test5 = operation(OperationType.SUBTRACT, AnyValue(2.75), AnyValue(1))
+    check(test5.first == OperationStatus.VALID && test5.second?.doubleValue() == 1.75) { "2.75 - 1 is 1.75" }
+
+    val test6 = operation(OperationType.SUBTRACT, AnyValue(2.75), AnyValue("merry christmas!"))
+    check(test6.first == OperationStatus.INVALID) { "double and string cannot be subtracted" }
+
+    val test7 = operation(OperationType.ADD, AnyValue("Happy Holidays"), AnyValue(1))
+    check(test7.first == OperationStatus.INVALID) { "Int and String cannot be added together" }
 }
--- docs/week05/task-project/src/main/kotlin/tasks.kt
+++ docs/week05/task-project/src/main/kotlin/tasks.kt
@@ -3,17 +3,75 @@
 //  Week 5 Tasks
 //
 //  You need to write code to complete the functions below to complete each task.
-//  You can develop and test each function individually, but some sequentially
-//  depend on the prior tasks.
-//  As long as a function returns nil, it is assumed to not be implemented.
 //
-//
-//  Due to the tests we need to perform, some lines in main.swift may generate
-//  warning errors. If you get an unexplained warning error from main.swift, please
-//  check if there is a comment near that line saying to ignore warning errors.
+//  task4() is meant to be open-ended. There are probably many ways to solve it. Use creativity
+//  to come to a good solution
 //
 
-import java.util.*
+enum class IntOperationType {
+    ADD, SUBTRACT, MULTIPLY, DIVIDE
+}
+
+typealias IntOperation = (Int, Int) -> Int
+
+typealias IntOperationMap = Map<IntOperationType, IntOperation>
+
+//  Task 0
+//
+//  Lambda functions can be stored in a Map, just like any other type of variable
+//
+//  In this task, create a Map that contains operations for all types in IntOperationType
+//  The key of the map will be an IntOperationType, and the value will be an IntOperation
+fun task0(): IntOperationMap {
+    return mapOf(IntOperationType.ADD to { a, b -> a + b },
+                 IntOperationType.SUBTRACT to { a, b -> a - b },
+                 IntOperationType.MULTIPLY to { a, b -> a * b },
+                 IntOperationType.DIVIDE to { a, b -> a / b })
+}
+
+typealias IntResult = () -> Int
+
+//  Task 1
+//
+//  Higher-order functions are functions that... wait for it... return another function
+//
+//  In this task, return a function that, when called, will add the two numbers passed _into_ the higher order function
+fun task1(a: Int, b: Int): IntResult? {
+    return { a + b }
+}
+
+//  Task 2
+//
+//  Create a higher order function that simple executes the parameter that also is a function.
+//  This gets _really_ confusing, so here is the best description I can come up with:
+//      - task2 is a function, which has a single parameter: a function that returns an Int
+//      - task2 returns a function, that when called, will return an Int
+//      - So the return value of task2 is a function that should return the result of the parameter function
+//
+//  Is your head spinning yet?
+fun task2(execute: () -> Int): IntResult? {
+    return { execute() }
+}
+
+//  Task 3
+//
+//  Like task2, you'll call the passed in function parameters and do an operation on them
+//  The operation you do needs to be based on the "op" parameter
+fun task3(op: IntOperationType, first: () -> Int, second: () -> Int): IntResult? {
+    if (op == IntOperationType.ADD) {
+        return { first() + second() }
+    }
+
+    if (op == IntOperationType.SUBTRACT) {
+        return { first() - second() }
+    }
+
+    if (op == IntOperationType.MULTIPLY) {
+        return { first() * second() }
+    }
+
+    return { first() / second() }
+}
 
 class AnyValue {
 
@@ -22,6 +80,15 @@ class AnyValue {
     constructor(s: String) { value = s }
     constructor(i: Int) { value = i }
     constructor(d: Double) { value = d }
+
+    val isString: Boolean
+        get() = stringValue() != null
+
+    val isInt: Boolean
+        get() = intValue() != null
+
+    val isDouble: Boolean
+        get() = doubleValue() != null
 
     fun stringValue(): String? {
         if (value is String) {
@@ -48,20 +115,77 @@ class AnyValue {
     }
 }
 
-//  Task 0
-//
-//  Lambda functions in Kotlin are first-class citizens, meaning they can be assigned
-//  to variables and passed as a parameter to a function.
-//
-//
+enum class OperationType {
+    ADD, SUBTRACT, CONCATENATE
+}
 
 enum class OperationStatus {
     VALID, INVALID
 }
 
-typealias Operation = (AnyValue, AnyValue) -> Pair<OperationStatus, AnyValue>
-fun task0(): Operation {
-    return { a: AnyValue, b: AnyValue ->
-        Pair(OperationStatus.VALID, AnyValue(10))
+//  Task 4
+//
+//  Lambda functions in Kotlin are first-class citizens, meaning they can be assigned
+//  to variables and passed as a parameter to a function.
+//
+//  For task0, you should return an OperationHandler with the following rules:
+//      - Int and Number types can be Added and Subtracted
+//      - String types can be concatenated
+//      - Strings _cannot_ be added and subtracted
+//      - Int and Number types _cannot_ be concatenated
+//      - If an Int and Double have an operation applied, the result should _be_ a Double
+//
+//  If the values can have an operation applied to them, return a OperationStatus.VALID, as well as the result
+//  If the values cannot have the operation applied, return OperationStatus.INVALID in the Pair, along with null
+//
+//  To make things easier to code, you should add several methods and computed properties to AnyValue,
+//  such as the following for string types:
+//      - fun stringValue(): String?
+//      - val isString: Boolean
+
+typealias OperationHandler = (OperationType, AnyValue, AnyValue) -> Pair<OperationStatus, AnyValue?>
+fun task4(): OperationHandler? {
+
+    return { op: OperationType, a: AnyValue, b: AnyValue ->
+
+        var result: Pair<OperationStatus, AnyValue?> = Pair(OperationStatus.INVALID, null)
+
+        if (a.isString && b.isString && op == OperationType.CONCATENATE) {
+            result = Pair(OperationStatus.VALID, AnyValue(a.stringValue() + b.stringValue()))
+        }
+
+        if (a.isInt && b.isInt && op == OperationType.ADD) {
+            result = Pair(OperationStatus.VALID, AnyValue((a.intValue() ?: 0) + (b.intValue() ?: 0)))
+        }
+
+        if (a.isInt && b.isInt && op == OperationType.SUBTRACT) {
+            result = Pair(OperationStatus.VALID, AnyValue((a.intValue() ?: 0) - (b.intValue() ?: 0)))
+        }
+
+        if (a.isDouble && b.isDouble && op == OperationType.ADD) {
+            result = Pair(OperationStatus.VALID, AnyValue((a.doubleValue() ?: 0.0) + (b.doubleValue() ?: 0.0)))
+        }
+
+        if (a.isInt && b.isInt && op == OperationType.SUBTRACT) {
+            result = Pair(OperationStatus.VALID, AnyValue((a.intValue() ?: 0) - (b.intValue() ?: 0)))
+        }
+
+        if (a.isInt && b.isDouble && op == OperationType.ADD) {
+            result = Pair(OperationStatus.VALID, AnyValue((a.intValue() ?: 0.0).toDouble() + (b.doubleValue() ?: 0.0)))
+        }
+
+        if (a.isInt && b.isDouble && op == OperationType.SUBTRACT) {
+            result = Pair(OperationStatus.VALID, AnyValue((a.intValue() ?: 0.0).toDouble() - (b.doubleValue() ?: 0.0)))
+        }
+
+        if (a.isDouble && b.isInt && op == OperationType.ADD) {
+            result = Pair(OperationStatus.VALID, AnyValue((a.doubleValue() ?: 0.0) + (b.intValue() ?: 0).toDouble()))
+        }
+
+        if (a.isDouble && b.isInt && op == OperationType.SUBTRACT) {
+            result = Pair(OperationStatus.VALID, AnyValue((a.doubleValue() ?: 0.0) - (b.intValue() ?: 0).toDouble()))
+        }
+
+        result
     }
 }
